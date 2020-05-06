e8c57bf8bdb6ae41f988c1927a24a7450446f4d1
Koji Onishi <fursich0@gmail.com>
2020-05-06 16:10:27 +0900

    ensure to align stack address when creating a local frame

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 int stack_size;
 
+static int align_to(int n, int base) {
+  assert((base & (base - 1)) == 0);
+  return (n + base - 1) & ~(base - 1);
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません: %s\n", argv[0]);
@@ -17,7 +22,7 @@ int main(int argc, char **argv) {
     offset += 8;
     var->offset = offset;
   }
-  block->stack_size = offset;
+  block->stack_size = align_to(offset, 16);
 
   codegen(block);
   return 0;
