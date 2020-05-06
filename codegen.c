f0cc6247c86ec5313cd1041f16a25195f615a5cd
Koji Onishi <fursich0@gmail.com>
2020-05-06 15:20:36 +0900

    refactor var accessor into store(), load(), and gen_addr()

--- codegen.c
+++ codegen.c
@@ -6,35 +6,43 @@
 
 static int labelseq = 1;
 
-static void gen_lval(Node *node) {
+static void gen_addr(Node *node) {
   if (node->kind != ND_VAR)
-    error("代入の左辺値が変数ではありません\n");
+    error("左辺値ではありません\n");
 
   printf("  mov rax, rbp\n");
   printf("  sub rax, %d\n", node->var->offset);
   printf("  push rax\n");
 }
 
+static void load() {
+  printf("  pop rax\n");
+  printf("  mov rax, [rax]\n");
+  printf("  push rax\n");
+}
+
+static void store() {
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+  printf("  mov [rax], rdi\n");
+  printf("  push rdi\n");
+}
+
 static void gen_expr(Node *node) {
   switch (node->kind) {
   case ND_ASSIGN:
-    gen_lval(node->lhs);
+    gen_addr(node->lhs);
     gen_expr(node->rhs);
 
-    printf("  pop rdi\n");
-    printf("  pop rax\n");
-    printf("  mov [rax], rdi\n");
-    printf("  push rdi\n");
+    store();
     return;
   case ND_NUM:
     printf("  push %d\n", node->val);
     return;
   case ND_VAR:
-    gen_lval(node);
+    gen_addr(node);
 
-    printf("  pop rax\n");
-    printf("  mov rax, [rax]\n");
-    printf("  push rax\n");
+    load();
     return;
   }
 
